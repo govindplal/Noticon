7563be73d1f8abf501cfc515d867fa9e029f411e
Govind P Lal <govindplal@gmail.com>
2024-04-03 13:26:26 +0530

    Landing page

---
+++ app/_components/navbar.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import { ModeToggle } from "@/components/mode-toggle"
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+
+export const Navbar = () => {
+ return (
+    <div className="flex flex-row justify-between px-6 pt-6">
+        <Image
+            src = "/logo.svg"
+            height="100"
+            width="100"
+            className='object-contain'
+            alt='logo' />
+            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                <Button asChild size="sm">
+                  <Link href="/documents">Get Noticon Free</Link>
+                </Button>
+        </div>
+        <div>
+            <ModeToggle/>
+        </div>
+    </div>
+  )
+}
---
+++ app/documents/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from '../_components/navigation'
+
+const dashboard = () => {
+  return (
+    <div>
+      <Navigation/>
+    </div>
+  )
+}
+
+export default dashboard
